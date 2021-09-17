cb4cc3ebb06ace56c6a7d1c6aa839ffdbaa4757d
Paige Morton <paigeashley624@gmail.com>
2021-09-17 10:40:57 -0400

    like button created and turns purple

--- src/App.js
+++ src/App.js
@@ -18,6 +18,16 @@ class App extends React.Component {
       photos: []
     }
   }
+
+  handleClick(index) {
+    let photos = [...this.state.photos]
+    let photo = {...photos[index]}
+    photo.liked = true 
+    photos[index] = photo 
+    this.setState({photos})
+    console.log(photo)
+  }
+
   componentDidMount() {
     axios.get(`https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=${api_key}`).then((response) => {
       // console.log(response) 
@@ -34,13 +44,17 @@ class App extends React.Component {
       <div className="App">
         <header className="App-header">
           {
-            this.state.photos.map((scopeVariablePhoto) => {
+            this.state.photos.map((scopeVariablePhoto, index) => {
+              
               return(
                 //add title of image, date of capture, rover name, make image smaller
                 <div>
                   <div key={scopeVariablePhoto.id}> 
+                    <h3>{scopeVariablePhoto.rover.name} Rover</h3>
+                    <h4>{scopeVariablePhoto.camera.name}({scopeVariablePhoto.camera.full_name})</h4>
+                    <p>Image Date: {scopeVariablePhoto.earth_date}</p>
                     <img src={scopeVariablePhoto.img_src} alt={this.photoAlt(scopeVariablePhoto)}></img>
-                    <h3>{scopeVariablePhoto.camera.name}</h3> 
+                    <button style={scopeVariablePhoto.liked && {color: 'purple'}} onClick={() => this.handleClick(index)}>Like</button>
                     
                   </div>
                 </div>
